33e19388fc9a974d2ec8f4bb95c9f1fd739330af
Amir Mullagaliev <amirmullagaliev@gmail.com>
2020-03-09 14:27:31 -0700

    Added Runge-Kutta method

--- integral.py
+++ integral.py
@@ -20,7 +20,7 @@ def trapeze(a, b, func, n=100):
     return s * h
 
 
-def rectangle(a, b, func, n = 100):
+def rectangle(a, b, func, n=100):
     # c === 1
     h = abs(b - a) / n
     s = 0
@@ -31,8 +31,7 @@ def rectangle(a, b, func, n = 100):
     return s * h
 
 
-def simpson(a, b, func, n = 100):
-
+def simpson(a, b, func, n=100):
     # gives 1, 4, 2, 4, ..., 2, 4, 1
 
     def coeff(n):
@@ -40,7 +39,6 @@ def simpson(a, b, func, n = 100):
         for i in range(n - 1):
             yield (4, 2)[i % 2]
         yield 1
-    
 
     x = a
     h = abs(b - a) / n
@@ -49,11 +47,11 @@ def simpson(a, b, func, n = 100):
     for c in coeff(n):
         s += c * func(x)
         x += h
-    
+
     return s * (h / 6) * 2
 
 
-def gauss(a, b, func, n = 100):
+def gauss(a, b, func, n=100):
     h = abs(b - a) / n
     h_2 = h / 2
     h_3 = h / (2 * math.sqrt(3))
